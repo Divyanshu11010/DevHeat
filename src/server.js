ceb1552726aa53f168bc7bbfd548f1a95a183955
Divyanshu Vishwakarma <sv090999@gmail.com>
2025-04-07 01:23:58 +0530

    port changed in server

--- src/server.js
+++ src/server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use("/api", endpoints);
 
 app.listen(3000, () => {
-    console.log("Server is running on port : 5000");
+    console.log("Server is running on port : 3000");
 });
